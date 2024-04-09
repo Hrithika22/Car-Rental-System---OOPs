8afb21c6a91280a1b3719672dd68545ffe78421a
Hrithika Gowlikar <hrithikagowlikar22@gmail.com>
2024-04-10 00:37:22 +0530

    Update add car return car

--- src/Main.java
+++ src/Main.java
@@ -86,7 +86,55 @@ class Rental{
     }
 }
 
+class CarRentalSystem{
+    private List<Car> cars;
+    private List<Customer> customers;
+    private List<Rental> rentals;
 
+    public CarRentalSystem(){
+        cars = new ArrayList<>();
+        customers = new ArraysList<>();
+        rentals = new ArrayList<>();
+    }
+
+    public void addCar(Car car){
+        cars.add(car);
+    }
+    public void addCustomer(Customer customer){
+        customers.add(customer);
+    }
+
+    public void rentCar(Car car, Customer customer, int days){
+        if(car.isAvailable()){
+            car.rent();
+            rentals.add(new Rental(car, customer, days));
+        }
+        else{
+            System.out.println("Sorry!! Car is not available for rent.");
+        }
+    }
+
+    public void returnCar(Car car){
+        car.returnCar();
+        Rental rentalToRemove = null;
+        for(Rental rental: rentals){
+            if(rental.getCar()==car){
+                rentalToRemove = rental;
+                break;
+            }
+        }
+        if(rentalToRemove!=null){
+            rentals.remove(rentalToRemove);
+        }
+        else{
+            System.out.println("Car was not rented.");
+
+        }
+    }
+
+
+    
+}
 
 
 
